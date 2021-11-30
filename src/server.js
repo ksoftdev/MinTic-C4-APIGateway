ddb80418bb68296f74f719ad4a2ad4a4c4f5d294
ksoftdev <kuntursoftdev@gmail.com>
2021-11-30 08:27:07 -0500

    02. Athentication, Resolvers, Typedefs, dataSources

---
+++ src/server.js
@@ -0,0 +1,6 @@
+module.exports = {
+    auth_api_url: 'https://g8-p10-biblioteca.herokuapp.com/',
+    account_api_url: 'https://ms-account-g4.herokuapp.com/',
+    biblio_api_url: 'https://biblioms.herokuapp.com/api/',
+    store_api_url: '',
+};
