1924cae1825820b9b0beefc0b35947982ae365b0
dialejo <email>
2021-12-05 20:37:55 -0500

    update

--- src/dataSources/biblio_api.js
+++ src/dataSources/biblio_api.js
@@ -10,7 +10,7 @@ class BiblioAPI extends RESTDataSource {
         return await this.post('/api/books/create', Book);
     }
     async querybook(book_id) {
-        return await this.get(`/api/books/${book_id}`);
+        return await this.get('/api/books/${book_id}');
     }
 
     async querybooks() {
